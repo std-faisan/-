070ab6a630ada75845eb0d4363d268952cb79dd4
std-faisan <s66309010008@ptnicec.ac.th>
2023-12-21 18:18:03 +0700

    21/12/66

--- work_handling_result.php
+++ work_handling_result.php
@@ -1,10 +1,34 @@
 <?php
-$Mode = $_POST['Mode'];
-$text1 = $_POST['teet1'];
-$text2 = $_POST['teet2'];
-$text3 = $_POST['teet3'];
+$fn = $_POST['NameFile'];
+$m = $_POST ['mode'];
+$Text1 = $_POST['Text1'];
+$Text2 = $_POST ['Text2'];
+$Text3 = $_POST ['Text3'];
 
 
 
+switch ($m) {
+    case 'r':
+        $ff = fopen($fn,$m) or die("ไม่สามารถสร้างได้");
+        echo fread($ff,filesize($fn));
+        break;
+    case 'w':
+        $ff = fopen($fn,$m) or die("ไม่สามารถสร้างได้");
+        fwrite($ff,$Text1."\n");
+        fwrite($ff,$Text2."\n");
+        fwrite($ff,$Text3."\n");
+        fclose($ff);
+        header('location: work_filehanling.html');
+        break;
 
+   case 'a':
+        $ff = fopen($fn,$m) or die("ไม่สามารถสร้างได้");
+        fwrite($ff,$Text1."\n");
+        fwrite($ff,$Text2."\n");
+        fwrite($ff,$Text3."\n");
+        fclose($ff);
+        header('location: work_filehanling.html');
+        break;
+
+}   
 ?>
